b164f04468df6721816dbdb6547ba68fa9a2fd8b
Anchit Aggarwal <anchitaggarwal506@gmail.com>
2021-09-24 11:24:24 +0530

    Create Implementation of Queue.java

---
+++ Implementation of Queue.java
@@ -0,0 +1,30 @@
+public class Queue {
+	
+	private ListNode front;
+	private ListNode rear;
+	private int length;
+	
+	private class ListNode {
+		private int data; // Can be a generic type
+		private ListNode next; // Reference to next ListNode in list
+		
+		public ListNode(int data) {
+			this.data = data;
+			this.next = null;
+		}
+	}
+	
+	public Queue() {
+		front = null;
+		rear = null;
+		length = 0;
+	}
+	
+	public int length() {
+		return length;
+	}
+	
+	public boolean isEmpty() {
+		return length == 0;
+	}
+}
